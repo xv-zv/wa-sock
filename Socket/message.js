305a6b0a68c2d6cc633e1ce92143e1c62ffc8e50
xv-zv <sv.yos.n@gmail.com>
2025-12-05 17:55:15 -0500

    update message.js

--- Socket/message.js
+++ Socket/message.js
@@ -1,5 +1,18 @@
+import {
+   isJidGroup,
+   isLidUser,
+   jidNormailezedUser,
+   getContentType
+} from 'baileys';
+
 async function fetchMessage(sock, ctx, quote) {
-   console.log(ctx)
+   
+   const from = jidNormailezedUser(ctx.key.remoteJid)
+   const isGroup = isJidGroup(from)
+   const isLidFrom = isLidUser(from)
+   const ids = Object.values(ctx.key).filter(i => /\d+@\D/.test(i))
+   
+      console.log(ctx , ids)
    return {}
 }
 export default fetchMessage
