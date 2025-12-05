28ea667d94d38fc5adc3ce879590e1a33d089646
xv-zv <sv.yos.n@gmail.com>
2025-12-05 18:20:18 -0500

    update message.js

--- Socket/message.js
+++ Socket/message.js
@@ -5,7 +5,13 @@ import {
    jidNormalizedUser,
    getContentType
 } from 'baileys';
-const toObject = obj =>  obj && { [obj]: obj }
+
+const toObject = obj => {
+   const [key, val] = Object.entries(obj)[0]
+   return !Boolean(val) ? {} : {
+      [key]: val
+   }
+}
 
 async function fetchMessage(sock, ctx, quote) {
    
@@ -15,10 +21,20 @@ async function fetchMessage(sock, ctx, quote) {
    const ids = Object.values(ctx.key).filter(i => /\d+@\D/.test(i))
    const user_pn = ids.find(i => isJidUser(i))
    const user_lid = ids.find(i => isLidUser(i))
+   
    let m = {
-      from, 
-      ...toObject(user_pn),
-      ...toObject(user_lid)
+      from,
+      ...toObject({ user_pn }),
+      ...toObject({ user_lid }),
+      ...toObject({ name: ctx.pushName })
+   }
+   
+   const type = getContentType(ctx.message)
+   const msg = ctx.message[type] || {}
+   const body = (typeof msg === 'string') ? msg : msg.caption || msg.text || ''
+   
+   if (body) {
+      const cmd = 'null'
    }
    
    console.log(ctx, m)
