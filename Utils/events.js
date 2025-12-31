d6ba74292b23928f47e45b02332b38f4a6056d23
xv-zv <sv.yos.n@gmail.com>
2025-12-31 09:46:57 -0500

    update events.js

--- Utils/events.js
+++ Utils/events.js
@@ -1,16 +1,18 @@
 export class Events {
    #events = {}
    on = (event, listner) => {
-      if (this.#events[event]) return
+      if (this.#events[event]) return this
       this.#events[event] = listner
+      return this
    }
    off = (event, ...args) => {
       this.emit(event, ...args)
       delete this.#events[event]
    }
    cmd = (cmd, listner) => {
-      if (this.cmds[cmd]) return
+      if (this.cmds[cmd]) return 
       this.cmds[cmd] = listner
+      return 
    }
    emit = (event, ...args) => {
       if (!this.#events[event]) return
