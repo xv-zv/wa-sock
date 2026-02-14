af1888e2e241c1b34309fd545c4629a4c4c41a87
xv-zv <sv.yos.n@gmail.com>
2026-02-14 18:17:49 -0500

    update group.js

--- Socket/Message/group.js
+++ Socket/Message/group.js
@@ -2,31 +2,32 @@ import { groupCache } from '#cache';
 
 export class Group {
    #id = null
+   
    constructor(id, options = {}) {
       this.#id = id
       this.parse(options)
    }
-   parse(opc) {
-      if (!groupCache.has(this.#id)) return
+   
+   parse(opc = {}) {
       const group = groupCache.get(this.#id)
+      if (!group) return
+      
       for (const key in group) {
          if (['users', 'desc'].includes(key)) continue
          this[key] = group[key]
       }
-      const admins = group.user.filter(i => i.admin)
+      
+      const admins = group.users.filter(i => i.admin).map(i => i.id)
+      
       this.isAdmin = admins.includes(opc.sender_id)
       this.isBotAdmin = admins.includes(opc.user_id)
    }
+   
    get users() {
-      if (groupCache.has(this.#id)) {
-         return groupCache.get(this.#id).users
-      }
-      return []
+      return groupCache.get(this.#id)?.users || []
    }
+   
    get desc() {
-      if (groupCache.has(this.#id)) {
-         return groupCache.get(this.#id).desc
-      }
-      return undefined
+      return groupCache.get(this.#id)?.desc
    }
 }
